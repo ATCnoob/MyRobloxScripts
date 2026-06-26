d6a3d60af7123b8bd4a3a3f84eea78307e18d87e
ATCnoob <dikmun8633@gmail.com>
2026-06-26 17:00:35 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -77,48 +77,55 @@ local function pressKey(keyName)
 	VirtualInputManager:SendKeyEvent(false, keyCode, false, game)
 end
 
+-- Spam E
 task.spawn(function()
-	while true do
-		if AutoBoss and character then
-			local boss4 = workspace.RefreshPoints.NPC:FindFirstChild("boss4Spawn1")
-			local boss5 = workspace.RefreshPoints.NPC:FindFirstChild("boss5Spawn1")
+    while true do
+        if AutoBoss then
+            pressKey("E")
+        end
+        task.wait(0.1)
+    end
+end)
+
+-- Spam Space
+task.spawn(function()
+    while true do
+        if AutoBoss then
+            pressKey("Space")
+        end
+        task.wait(0.1)
+    end
+end)
+
+-- Boss loop
+task.spawn(function()
+    while true do
+        if AutoBoss and character then
 			local boss6 = workspace.RefreshPoints.NPC:FindFirstChild("boss6Spawn1")
-			local boss7 = workspace.RefreshPoints.NPC:FindFirstChild("boss7Spawn1")
-			local boss8 = workspace.RefreshPoints.NPC:FindFirstChild("boss8Spawn1")
-			local boss9 = workspace.RefreshPoints.NPC:FindFirstChild("boss9Spawn1")
+            local boss8 = workspace.RefreshPoints.NPC:FindFirstChild("boss8Spawn1")
+            local boss9 = workspace.RefreshPoints.NPC:FindFirstChild("boss9Spawn1")
 
-			-- Spam E
-			task.spawn(function()
-				-- Spam E
-        		while true do
-            		pressKey("E")
-            		task.wait(0.1)
-        		end
-    		end)
+            if boss8 then
+                character:PivotTo(boss8.CFrame)
+            end
 
-    		-- Spam Space
-    		task.spawn(function()
-       			while true do
-            	pressKey("Space")
-            	task.wait(0.1)
-        		end
-    		end)
+            task.wait(2)
 
-			if boss8 and AutoBoss then
-				character:PivotTo(boss8.CFrame)
-			end
+            if boss9 then
+                character:PivotTo(boss9.CFrame)
+            end
 
 			task.wait(2)
 
-			if boss9 and AutoBoss then
-				character:PivotTo(boss9.CFrame)
+			if boss6 then
+				character:PivotTo(boss6.CFrame)
 			end
 
-			task.wait(2)
-		else
-			task.wait(1)
-		end
-	end
+            task.wait(2)
+        else
+            task.wait(1)
+        end
+    end
 end)
 
 --------------------------------------------------
